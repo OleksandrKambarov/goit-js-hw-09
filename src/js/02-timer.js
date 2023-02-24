5b3c11dafd1d183ad9edaaa282a4b52cbfb8dc14
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-02-24 12:11:53 +0200

    let getRef+

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,3 +2,28 @@ import flatpickr from 'flatpickr';
 import convertMs from './dateConvert';
 
 import 'flatpickr/dist/flatpickr.min.css';
+
+let getRef = selector => document.querySelector(selector);
+const imputDatePickerRef = getRef('#datetime-picker');
+const btnStartRef = getRef('[data-start]');
+const daysRef = getRef('[data-days]');
+const hoursRef = getRef('[data-hours]');
+const minutesRef = getRef('[data-minutes]');
+const secondsRef = getRef('[data-seconds]');
+
+let timeDifference = 0;
+let timerId = null;
+let formatDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    currentDifferenceDate(selectedDates[0]);
+  },
+};
+
+btnStartRef.setAttribute('disabled', true);
