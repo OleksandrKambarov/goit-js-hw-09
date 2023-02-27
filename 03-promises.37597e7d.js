28fd3ed3257bc93fd76db0f72728d14054ac9e34
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-02-27 10:36:31 +0000

    Deploying to gh-pages from @ OleksandrKambarov/goit-js-hw-09@ee6701a998c323bd65cc9365dadc1c4155d172f9 🚀

--- 03-promises.37597e7d.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form");function t(e,t){Math.random()}e.addEventListener("submit",(function(o){o.preventDefault();let i=Number(e.delay.value);for(let o=1;o<=e.amount.value;o+=1)t(o,i).then((({position:e,delay:t})=>{Notify.success(`✅ Fulfilled promise ${e} in ${t}ms`)})).catch((({position:e,delay:t})=>{Notify.failure(`❌ Rejected promise ${e} in ${t}ms`)})),i+=Number(e.step.value)}));
-//# sourceMappingURL=03-promises.37597e7d.js.map
