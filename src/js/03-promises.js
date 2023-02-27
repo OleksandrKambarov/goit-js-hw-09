a5530f30928fe52a7c560483ef92c5d9ebb90f1f
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-02-27 12:33:16 +0200

    function onSubmitForm

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,23 @@
 const formRef = document.querySelector('.form');
 
+formRef.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  let delay = Number(formRef.delay.value);
+
+  for (let i = 1; i <= formRef.amount.value; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += Number(formRef.step.value);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
